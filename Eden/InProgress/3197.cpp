a1fd941fc786507b28e7b881ef2fd254064f2c72
김건형 <eden@gimgeonhyeongs-Laptop.local>
2024-05-30 20:10:46 +0900

    3197: change method

--- Eden/InProgress/3197.cpp
+++ Eden/InProgress/3197.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <queue>
 #include <algorithm>
+#include <cmath>
+#include <tuple>
 
 using namespace std;
 
@@ -8,26 +10,26 @@ class Lake {
     private: 
         int R, C;
         char** lake;
-        bool** vis;
-        bool** vis_swan;
+        int*** vis;
         int dx[4] = {1,0,-1,0};
         int dy[4] = {0,1,0,-1};
         pair<int,int> init_swan;
         pair<int,int> dest_swan;
-        queue<pair<int,int>> melt;
         
     public:
         Lake(int R, int C) {
             this->R = R;
             this->C = C;
             lake = (char**)malloc(sizeof(char*)*R);
-            vis = (bool**)malloc(sizeof(bool*)*R);
-            vis_swan = (bool**)malloc(sizeof(bool*)*R);
+            vis = (int***)malloc(sizeof(int**)*R);
 
             for (int i = 0; i < R; i++) {
                 lake[i] = (char*)malloc(sizeof(char)*C);
-                vis[i] = (bool*)malloc(sizeof(bool)*C);
-                vis_swan[i] = (bool*)malloc(sizeof(bool)*C);
+                vis[i] = (int**)malloc(sizeof(int*)*C);
+                for (int j = 0; j < C; j++)
+                {
+                    vis[i][j] = (int*)malloc(sizeof(int)*max(R,C));
+                }
             }
         }
 
@@ -48,22 +50,11 @@ class Lake {
                         else
                             dest_swan = make_pair(i,j);
                     }
-                    bool queueflag = false;
-                    if (lake[i][j] == '.') {
-                        for (int d = 0; d < 4; d++)
-                        {
-                            if (lake[i+dx[d]][j+dy[d]] == 'X'){
-                                queueflag = true;
-                                break;
-                            }
-                        }
-                        if (queueflag) {
-                            this->melt.push(make_pair(i,j));
-                        }
-                    }
                 }
-                fill(vis[i], vis[i]+C, 0);
-                fill(vis_swan[i], vis_swan[i]+C, 0);
+                for (int j = 0; j < C; j++)
+                {
+                    fill(vis[i][j], vis[i][j]+max(R,C), -1);
+                }
             }
         }
 
@@ -77,69 +68,54 @@ class Lake {
                 cout << '\n';
             }
         }
+
         void printSwanVis() {
             for (int i = 0; i < R; i++)
             {
                 for (int j = 0; j < C; j++)
-                    cout << vis_swan[i][j];
+                    cout << vis[i][j];
                 cout << '\n';
             }
         }
-
-        void tomorrow() {
-            // TODO: change this area into queue impl
-            for (int i = 0; i < R; i++)
-            {
-                for (int j = 0; j < C; j++)
-                {
-                    if (lake[i][j] != '.' || vis[i][j])
-                        continue;
-                    for (int d = 0; d < 4; d++)
-                    {
-                        int nx = i+dx[d];
-                        int ny = j+dy[d];
-                        if (nx < 0 || nx >= R || ny < 0 || ny >= C)
-                            continue;
-                        if (lake[nx][ny] == 'X' && !vis[nx][ny])
-                        {
-                            lake[nx][ny] = '.';
-                            vis[nx][ny] = 1;
-                        }
-                    }
-                }
-            }
-            for (int i = 0; i < R; i++)
-            {
-                fill(vis[i], vis[i]+C, 0);
-            }
-        }
-        bool isConnected() {
-            queue<pair<int,int> > q;
-            q.push(init_swan);
-            vis_swan[init_swan.first][init_swan.second] = 1;
+        int isConnected() {
+            queue<tuple<int,int,int> > q;
+            q.push({init_swan.first, init_swan.second, 0});
+            vis[init_swan.first][init_swan.second] = 0;
 
             while(!q.empty())
             {
-                pair<int,int> p = q.front();
+                tuple<int,int,int> cur = q.front();
                 q.pop();
-                init_swan = p;
-                if (p == dest_swan)
-                    return true;
+                for (int i = 0; i < max(R,C); i++)
+                {
+                    if (vis[dest_swan.first][dest_swan.second][i] != -1)
+                    {
+                        return i;
+                    }
+                }
 
                 for (int d = 0; d < 4; d++)
                 {
-                    int nx = p.first + dx[d];
-                    int ny = p.second + dy[d];
+                    int nx = get<0>(cur) + dx[d];
+                    int ny = get<1>(cur) + dy[d];
+                    int nz = get<2>(cur);
 
                     if (nx < 0 || nx >= R || ny < 0 || ny >= C)
                         continue;
-                    if (lake[nx][ny] == 'X')
+                    if (vis[nx][ny][nz] != -1)
+                    {
                         continue;
-                    if (vis_swan[nx][ny])
-                        continue;
+                    }
                     
-                    q.push(make_pair(nx,ny));
-                    vis_swan[nx][ny] = 1;
+                    if (lake[nx][ny] == 'X') {
+                        q.push(tuple(nx,ny,nz+1));
+                        vis[nx][ny][nz+1] = vis[get<0>(cur)][get<1>(cur)][get<2>(cur)];
+                    }
+                    else
+                    {
+                        q.push(tuple(nx,ny,nz));
+                        vis[nx][ny][nz] = vis[get<0>(cur)][get<1>(cur)][get<2>(cur)];
+                    }
                 }
             }
             // this->printSwanVis();
@@ -161,16 +137,9 @@ int main()
 
     Lake l = Lake(r, c);
     l.init();
-    
     int ans = 0;
-    // cout << l.isConnected() << '\n';
-
-    while (true) {
-        if (l.isConnected())
-            break;
-        l.tomorrow();
-        ans++;
-    }
+    ans = ceil((double)l.isConnected()/2);
+    // l.printSwanVis();
 
     cout << ans;
 
